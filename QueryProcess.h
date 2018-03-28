9419f7cfad00055092c252392c18fe5054e1d75f
chen <chenljian@163.com>
2018-03-28 19:42:47 +0800

    init all steps

--- QueryProcess.h
+++ QueryProcess.h
@@ -1,8 +1,17 @@
+#include<iostream>
+
+#define MAX_RELATION_NUM 10
+
 class QueryProcess
 {
 public:
-	int relationArray[10];
-	QueryTree queryTree;
-private:
+	int relationArray[MAX_RELATION_NUM];	//map relation 
+	void* relationAddr[MAX_RELATION_NUM];	//relation file/mem address
+	QueryTree* pQueryTree;
 
+private:
+	buildRelation(char *fileName);	//return the No. of fileName
+	buildQueryTree(char* cmdline);	//build QueryTree according to cmdline
+	executionQuery();
+	calculateResult();		//return sum
 }
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 #SigmodContestJoin
+steps:
+  1. build map: mem<-->file;
+  2. ready for query;
+  3. cmd parser;
+  4. query(Join, Select, Project);
+  5. return result.
